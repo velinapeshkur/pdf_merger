bb578f502fc4b7f84cafddc96769343f2854aff2
velinapeshkur <2611phoebe@gmail.com>
2026-05-25 19:21:49 +0300

    add custom font, change theme

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from app import merge_pdf
 
 
-sg.theme("LightBrown1")
+sg.theme("LightGrey1")
 
 label1 = sg.Text("Select folder:")
 input1 = sg.Input()
@@ -11,14 +11,15 @@ choose_button = sg.FolderBrowse("Choose", key="folder")
 label2 = sg.Text("Files per output file:")
 input2 = sg.Input(key="files")
 
-merge_button = sg.Button("Merge")
+merge_button = sg.Button("Merge", pad=(5,13))
 output_label = sg.Text(key="output")
 
 window = sg.Window("PDF Merger",
                    layout=[
                        [label1], [input1, choose_button],
                        [label2], [input2], 
-                       [merge_button, output_label]])
+                       [merge_button, output_label]],
+                   font=("San Francisco", 14))
 
 while True:
     event, values = window.read()
@@ -29,6 +30,7 @@ while True:
             folder_path = values["folder"]
             files_per_file = int(values["files"])
             merge_pdf(folder_path, files_per_file)
-            window["output"].update(value="Merge completed", text_color="green")
-
+            window["output"].update(value="Merge completed", 
+                                    text_color="green")
+            
 window.close()
